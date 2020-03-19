8f6f9b764474faa9433fd118b8422c6ec8dca2f4
rkdrud34 <rkdrud34@nate.com>
2020-03-19 20:42:45 +0900

    3-4

--- clock.js
+++ clock.js
@@ -2,15 +2,19 @@ const clockContainer = document.querySelector(".js-clock"),
     clockTitle = clockContainer.querySelector("h1");
 
 
-function getTime(){
-    const  date = new Date();
-    const minutes =  date.getMinutes();
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}: ${minutes}:${seconds}`
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}`:  minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
 }
-function init(){
+function init() {
     getTime()
+    setInterval(getTime, 1000);
 }
 const date = new Date()
 
