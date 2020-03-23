693b8a2138735a46aac154ed9057d2b532be764a
rkdrud34 <rkdrud34@nate.com>
2020-03-23 19:08:09 +0900

    path

---
+++ paintJS/app.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
+
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+
+}
+
+function startPainting() {
+    painting = true;
+
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        console.log('creating line in', x, y)
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+
+    } else {
+        console.log('creating line in', x, y)
+        ctx.lineTo(x, y);
+        ctx.stroke()
+    }
+
+}
+
+function onMouseDown(evnet) {
+    painting = true;
+    console.log(painting)
+
+
+};
+
+
+
+if (canvas) {
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
